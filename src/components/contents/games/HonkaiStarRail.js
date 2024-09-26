b4b2d51da2129d244c333b1b1f6377a333d19365
taufiqpop <taufiqpop999@gmail.com>
2024-09-26 10:39:31 +0700

    update

--- src/components/contents/games/HonkaiStarRail.js
+++ src/components/contents/games/HonkaiStarRail.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import GLightbox from 'glightbox';
+import 'glightbox/dist/css/glightbox.css';
 
 const HonkaiStarRail = () => {
+    useEffect(() => {
+        GLightbox({
+            selector: '.glightbox',
+        });
+    }, []);
+
     return (
-        <div>
-        <h1>HonkaiStarRail</h1>
-        </div>
+        <main className="main">
+            <section id="hero" className="hero section">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-6 text-center" data-aos="fade-up" data-aos-delay="50">
+                            <h2><span className="underlight">Honkai Star Rail</span></h2>
+                            <a href='https://hsr.hoyoverse.com/en-us/home' target='_blank' className="btn-get-started my-rating">My Rating ⭐ 9/10<br /></a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="gallery" className="gallery section">
+                <div className="container-fluid" data-aos="fade-up" data-aos-delay="200">
+                    <div className="row gy-4 justify-content-center">
+
+                        {/* Firefly */}
+                        <div className="col-xl-3 col-lg-4 col-md-6">
+                            <div className="gallery-item h-100">
+                                <img src="img/games/honkai-star-rail/Firefly.jpeg" className="img-fluid" alt="Firefly" />
+                                <div className="gallery-links d-flex align-items-center justify-content-center">
+                                    <div className="card-img-overlay d-flex align-items-center p-0">
+                                        <h5 className="card-title text-center flex-fill p-3 fs-4 waifu-overlay">
+                                            <small>Firefly</small>
+                                            <br />
+                                            <a href="img/games/honkai-star-rail/Firefly.jpeg" className="glightbox preview-link">(Honkai Star Rail)</a>
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 };
 
--- src/components/contents/games/WutheringWaves.js
+++ src/components/contents/games/WutheringWaves.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import GLightbox from 'glightbox';
+import 'glightbox/dist/css/glightbox.css';
 
 const WutheringWaves = () => {
+    useEffect(() => {
+        GLightbox({
+            selector: '.glightbox',
+        });
+    }, []);
+
     return (
-        <div>
-        <h1>WutheringWaves</h1>
-        </div>
+        <main className="main">
+            <section id="hero" className="hero section">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-6 text-center" data-aos="fade-up" data-aos-delay="50">
+                            <h2><span className="underlight">Wuthering Waves</span></h2>
+                            <a href='https://wutheringwaves.kurogames.com/en/main/' target='_blank' className="btn-get-started my-rating">My Rating ⭐ 9/10<br /></a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="gallery" className="gallery section">
+                <div className="container-fluid" data-aos="fade-up" data-aos-delay="200">
+                    <div className="row gy-4 justify-content-center">
+
+                        {/* Jinhsi */}
+                        <div className="col-xl-3 col-lg-4 col-md-6">
+                            <div className="gallery-item h-100">
+                                <img src="img/games/wuthering-waves/Jinhsi.jpeg" className="img-fluid" alt="Jinhsi" />
+                                <div className="gallery-links d-flex align-items-center justify-content-center">
+                                    <div className="card-img-overlay d-flex align-items-center p-0">
+                                        <h5 className="card-title text-center flex-fill p-3 fs-4 waifu-overlay">
+                                            <small>Jinhsi</small>
+                                            <br />
+                                            <a href="img/games/wuthering-waves/Jinhsi.jpeg" className="glightbox preview-link">(Wuthering Waves)</a>
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 };
 
