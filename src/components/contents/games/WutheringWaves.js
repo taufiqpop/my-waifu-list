b2efc91625d72a313ad40a76b0887450bbd86b9c
taufiqpop <taufiqpop999@gmail.com>
2024-09-28 13:06:36 +0700

    update

--- src/components/contents/games/WutheringWaves.js
+++ src/components/contents/games/WutheringWaves.js
@@ -40,7 +40,151 @@ const WutheringWaves = () => {
                                 </div>
                             </div>
                         </div>
-                        
+
+                        {/* Shorekeeper */}
+                        <div className="col-xl-3 col-lg-4 col-md-6">
+                            <div className="gallery-item h-100">
+                                <img src="img/games/wuthering-waves/Shorekeeper.gif" className="img-fluid" alt="Shorekeeper" />
+                                <div className="gallery-links d-flex align-items-center justify-content-center">
+                                    <div className="card-img-overlay d-flex align-items-center p-0">
+                                        <h5 className="card-title text-center flex-fill p-3 fs-4 waifu-overlay">
+                                            <small>Shorekeeper</small>
+                                            <br />
+                                            <a href="img/games/wuthering-waves/Shorekeeper.gif" className="glightbox preview-link">(Wuthering Waves)</a>
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Baizhi */}
+                        <div className="col-xl-3 col-lg-4 col-md-6">
+                            <div className="gallery-item h-100">
+                                <img src="img/games/wuthering-waves/Baizhi.gif" className="img-fluid" alt="Baizhi" />
+                                <div className="gallery-links d-flex align-items-center justify-content-center">
+                                    <div className="card-img-overlay d-flex align-items-center p-0">
+                                        <h5 className="card-title text-center flex-fill p-3 fs-4 waifu-overlay">
+                                            <small>Baizhi</small>
+                                            <br />
+                                            <a href="img/games/wuthering-waves/Baizhi.gif" className="glightbox preview-link">(Wuthering Waves)</a>
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Danjin */}
+                        <div className="col-xl-3 col-lg-4 col-md-6">
+                            <div className="gallery-item h-100">
+                                <img src="img/games/wuthering-waves/Danjin.gif" className="img-fluid" alt="Danjin" />
+                                <div className="gallery-links d-flex align-items-center justify-content-center">
+                                    <div className="card-img-overlay d-flex align-items-center p-0">
+                                        <h5 className="card-title text-center flex-fill p-3 fs-4 waifu-overlay">
+                                            <small>Danjin</small>
+                                            <br />
+                                            <a href="img/games/wuthering-waves/Danjin.gif" className="glightbox preview-link">(Wuthering Waves)</a>
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Sanhua */}
+                        <div className="col-xl-3 col-lg-4 col-md-6">
+                            <div className="gallery-item h-100">
+                                <img src="img/games/wuthering-waves/Sanhua.gif" className="img-fluid" alt="Sanhua" />
+                                <div className="gallery-links d-flex align-items-center justify-content-center">
+                                    <div className="card-img-overlay d-flex align-items-center p-0">
+                                        <h5 className="card-title text-center flex-fill p-3 fs-4 waifu-overlay">
+                                            <small>Sanhua</small>
+                                            <br />
+                                            <a href="img/games/wuthering-waves/Sanhua.gif" className="glightbox preview-link">(Wuthering Waves)</a>
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Yangyang */}
+                        <div className="col-xl-3 col-lg-4 col-md-6">
+                            <div className="gallery-item h-100">
+                                <img src="img/games/wuthering-waves/Yangyang.gif" className="img-fluid" alt="Yangyang" />
+                                <div className="gallery-links d-flex align-items-center justify-content-center">
+                                    <div className="card-img-overlay d-flex align-items-center p-0">
+                                        <h5 className="card-title text-center flex-fill p-3 fs-4 waifu-overlay">
+                                            <small>Yangyang</small>
+                                            <br />
+                                            <a href="img/games/wuthering-waves/Yangyang.gif" className="glightbox preview-link">(Wuthering Waves)</a>
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Encore */}
+                        <div className="col-xl-3 col-lg-4 col-md-6">
+                            <div className="gallery-item h-100">
+                                <img src="img/games/wuthering-waves/Encore.gif" className="img-fluid" alt="Encore" />
+                                <div className="gallery-links d-flex align-items-center justify-content-center">
+                                    <div className="card-img-overlay d-flex align-items-center p-0">
+                                        <h5 className="card-title text-center flex-fill p-3 fs-4 waifu-overlay">
+                                            <small>Encore</small>
+                                            <br />
+                                            <a href="img/games/wuthering-waves/Encore.gif" className="glightbox preview-link">(Wuthering Waves)</a>
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Rover */}
+                        <div className="col-xl-3 col-lg-4 col-md-6">
+                            <div className="gallery-item h-100">
+                                <img src="img/games/wuthering-waves/Rover.gif" className="img-fluid" alt="Rover" />
+                                <div className="gallery-links d-flex align-items-center justify-content-center">
+                                    <div className="card-img-overlay d-flex align-items-center p-0">
+                                        <h5 className="card-title text-center flex-fill p-3 fs-4 waifu-overlay">
+                                            <small>Rover</small>
+                                            <br />
+                                            <a href="img/games/wuthering-waves/Rover.gif" className="glightbox preview-link">(Wuthering Waves)</a>
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Camellya */}
+                        <div className="col-xl-3 col-lg-4 col-md-6">
+                            <div className="gallery-item h-100">
+                                <img src="img/games/wuthering-waves/Camellya.gif" className="img-fluid" alt="Camellya" />
+                                <div className="gallery-links d-flex align-items-center justify-content-center">
+                                    <div className="card-img-overlay d-flex align-items-center p-0">
+                                        <h5 className="card-title text-center flex-fill p-3 fs-4 waifu-overlay">
+                                            <small>Camellya</small>
+                                            <br />
+                                            <a href="img/games/wuthering-waves/Camellya.gif" className="glightbox preview-link">(Wuthering Waves)</a>
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Jianxin */}
+                        <div className="col-xl-3 col-lg-4 col-md-6">
+                            <div className="gallery-item h-100">
+                                <img src="img/games/wuthering-waves/Jianxin.gif" className="img-fluid" alt="Jianxin" />
+                                <div className="gallery-links d-flex align-items-center justify-content-center">
+                                    <div className="card-img-overlay d-flex align-items-center p-0">
+                                        <h5 className="card-title text-center flex-fill p-3 fs-4 waifu-overlay">
+                                            <small>Jianxin</small>
+                                            <br />
+                                            <a href="img/games/wuthering-waves/Jianxin.gif" className="glightbox preview-link">(Wuthering Waves)</a>
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </section>
